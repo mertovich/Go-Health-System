5acf09fb6c5e5ca62c526d89643c53746e41bb7d
mertovich <88425310+mertovich@users.noreply.github.com>
2022-02-18 11:46:32 +0300

    add(Menu): option menu

--- src/main.go
+++ src/main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"main/doctor"
 	"main/patient"
+	"strings"
 )
 
 func main() {
+	// page controller
+	var exit string
+
 	// dcotor data
 	d1 := doctor.Doctor{`Jhon`, 1}
 	d2 := doctor.Doctor{`Anna`, 2}
@@ -20,29 +24,38 @@ func main() {
 	var surname string
 	var expertise int
 
-	// panel
-	fmt.Print(`HealthNumber: `)
-	fmt.Scan(&HealthNumber)
-	fmt.Print(`Name: `)
-	fmt.Scan(&name)
-	fmt.Print(`Surname: `)
-	fmt.Scan(&surname)
-	fmt.Println(`[1] General Surgery`)
-	fmt.Println(`[2] Ophthalmologist`)
-	fmt.Print(`Expertise: `)
-	fmt.Scanf(`%d`, &expertise)
+	for {
+		// panel
+		fmt.Print(`HealthNumber: `)
+		fmt.Scan(&HealthNumber)
+		fmt.Print(`Name: `)
+		fmt.Scan(&name)
+		fmt.Print(`Surname: `)
+		fmt.Scan(&surname)
+		fmt.Println(`[1] General Surgery`)
+		fmt.Println(`[2] Ophthalmologist`)
+		fmt.Print(`Expertise: `)
+		fmt.Scanf(`%d`, &expertise)
 
-	// create patient
-	p1 := patient.Patient{}
-	p1.HealthNumber = HealthNumber
-	p1.Name = name
-	p1.Surname = surname
-	p1.Expertise = expertise
+		// create patient
+		p1 := patient.Patient{}
+		p1.HealthNumber = HealthNumber
+		p1.Name = name
+		p1.Surname = surname
+		p1.Expertise = expertise
 
-	// to compare
-	for _, d := range docList {
-		if d.Expertise == p1.Expertise {
-			fmt.Println(`Your doctor's name: ` + d.Name)
+		// to compare
+		for _, d := range docList {
+			if d.Expertise == p1.Expertise {
+				fmt.Println(`Your doctor's name: ` + d.Name)
+			}
+		}
+		fmt.Print(`[] Exit E/n: `)
+		fmt.Scanf(`%s`, &exit)
+		exit = strings.ToLower(exit)
+
+		if exit == `e` {
+			break
 		}
 	}
 }
